3b53ba2aff3b794452dd3601f1159e16a4ee4636
Orcan Tiryakioglu <orcan.tiryakioglu@gmail.com>
2020-03-25 07:27:56 +0300

    Refactor `s` arg of `recursive_sha256` to `hashsum`

--- src/data/integrity.py
+++ src/data/integrity.py
@@ -3,16 +3,16 @@ from pathlib import Path
 from typing import Any
 
 
-def recursive_sha256(path: Path, s: Any = None) -> str:
+def recursive_sha256(path: Path, hashsum: Any = None) -> str:
     if not path.exists():
         raise ValueError("Path does not exist")
 
-    s = hashlib.sha256() if not s else s
+    hashsum = hashlib.sha256() if not hashsum else hashsum
 
     if path.is_dir():
         for item in path.iterdir():
-            recursive_sha256(item, s)
+            recursive_sha256(item, hashsum)
     else:
-        s.update(path.read_bytes())
+        hashsum.update(path.read_bytes())
 
-    return str(s.hexdigest())
+    return str(hashsum.hexdigest())
