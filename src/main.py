77a2150e71a8f6b26f8a2c6f0d1716c6b0389b95
Orcan Tiryakioglu <orcan.tiryakioglu@gmail.com>
2020-05-02 17:54:46 +0300

    Add `Motion Sense` dataset support

--- src/main.py
+++ src/main.py
@@ -3,17 +3,18 @@ from pathlib import Path
 import toml
 
 from .data.datamanager import DataManager
-from .data.dataset import Wisdm
+from .data.dataset import Wisdm, MotionSense
 
 datasets = toml.load("config.toml")["dataset"]
 WISDM_PATH = Path(datasets["wisdm"])
+MOTION_SENSE_PATH = Path(datasets["motion-sense"])
 
 
 def main() -> None:
     #  wisdm_maker = WisdmMaker(WISDM_ROOT_PATH, INTERIM_ROOT)
     #  wisdm_maker.convert()
 
-    dataset = Wisdm(WISDM_PATH)
+    dataset = MotionSense(MOTION_SENSE_PATH)
     data_manager = DataManager(dataset)
 
     print(data_manager.read_schema())
