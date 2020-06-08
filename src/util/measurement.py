b31eefd7158adae8a046004989bf1e01733482c6
Orcan Tiryakioglu <orcan.tiryakioglu@gmail.com>
2020-06-08 20:15:29 +0300

    Removed dtw

--- src/util/measurement.py
+++ src/util/measurement.py
@@ -1,6 +1,5 @@
 from typing import DefaultDict, Dict, List, Optional, Sequence, Tuple, Union
 
-import dtw
 import numpy
 
 from ..data.window import NumpySequences
@@ -15,22 +14,6 @@ def min_euclidean(
         for sequences, _ in data:
             for sequence in sequences:
                 dist = numpy.linalg.norm(sequence - sample)
-                min_dist = min(min_dist, dist) if min_dist else dist  # type: ignore
-        distances.append(min_dist if min_dist else 0)
-    return sum(distances) / len(distances)
-
-
-def dynamic_time_warp(
-    samples: Union[numpy.ndarray, Sequence[numpy.ndarray]], data: NumpySequences,
-) -> float:
-    distances: List[float] = []
-    for sample in samples:
-        min_dist: Optional[float] = None
-        for sequences, _ in data:
-            for sequence in sequences:
-                dist = dtw.accelerated_dtw(
-                    sample, sequence, lambda a, b: numpy.linalg.norm(a - b)
-                )
                 min_dist = min(min_dist, dist) if min_dist else dist  # type: ignore
         distances.append(min_dist if min_dist else 0)
     return sum(distances) / len(distances)
